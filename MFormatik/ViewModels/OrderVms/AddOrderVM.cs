f5a78720816ede246906e786105fededb340a4e3
WAHID YATOUDJI <yattodji@gmail.com>
2025-05-30 15:13:55 +0100

    Enhance order management with product and client features
    Updated AddOrderVM to manage clients and products, added commands for saving and adding products. Modified AddOrderView for improved layout and bindings. Introduced ProductLineViewModel for handling individual product lines. Created ProductLineControl for displaying product details in the order.

--- MFormatik/ViewModels/OrderVms/AddOrderVM.cs
+++ MFormatik/ViewModels/OrderVms/AddOrderVM.cs
@@ -1,4 +1,10 @@
-﻿using MFormatik.Application.MediatorService;
+﻿using CommunityToolkit.Mvvm.Input;
+using MFormatik.Application.MediatorService;
+using MFormatik.Core.DTOs;
+using MFormatik.Core.Models;
+using MFormatik.ViewModels.ProductVms;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace MFormatik.ViewModels.OrderVms
 {
@@ -6,9 +12,120 @@ namespace MFormatik.ViewModels.OrderVms
     {
         private readonly IMediator _mediator;
 
+        private ClientDTO _selectedClient;
+        private Product _selectedProduct;
+        private ObservableCollection<ClientDTO> _clientsList;
+        private ObservableCollection<Product> _productList;
+
+
+        public ClientDTO SelectedClient
+        {
+            get { return _selectedClient; }
+            set
+            {
+                if (_selectedClient == value) return;
+                _selectedClient = value;
+                OnPropertyChanged();
+            }
+        }
+        public Product SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                if (_selectedProduct == value) return;
+                _selectedProduct = value;
+                OnPropertyChanged();
+            }
+        }
+        public ObservableCollection<ClientDTO> ClientsList
+        {
+            get { return _clientsList; }
+            set
+            {
+                if (_clientsList == value) return;
+                _clientsList = value;
+                OnPropertyChanged();
+            }
+        }
+        public ObservableCollection<Product> ProductsList
+        {
+            get { return _productList; }
+            set
+            {
+                if (_productList == value) return;
+                _productList = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public ObservableCollection<ProductLineViewModel> ProductLines { get; set; } = new ObservableCollection<ProductLineViewModel>();
+
+
+        public ICommand SaveCommand { get; }
+        public ICommand CancelCommand { get; }
+        public ICommand AddProductCommand { get; }
+
+
         public AddOrderVM(IMediator mediator)
         {
             _mediator = mediator;
+
+            _selectedClient = new();
+            _selectedProduct = new();
+            _clientsList = new ObservableCollection<ClientDTO>();
+            _productList = new ObservableCollection<Product>();
+            OrderItems = new ObservableCollection<OrderItem>();
+
+            SaveCommand = new RelayCommand(SaveOrder);
+            CancelCommand = new RelayCommand(CloseWindow);
+            AddProductCommand = new RelayCommand(AddProductToOrder);
+
+            LoadData();
+            ClearData();
+        }
+
+        private async void LoadData()
+        {
+            await LoadClients();
+            await LoadProducts();
+        }
+
+        private async Task LoadProducts() => ProductsList = await _mediator.ProductService.GetAllProductsAsync();
+
+        private async Task LoadClients() => ClientsList = await _mediator.ClientService.GetAllClientsASDtoAsync();
+
+        private void ClearData()
+        {
+            SelectedClient = null;
+            SelectedProduct = null;
+            Total = 0;
+            OrderDate = default;
+            OrderItems.Clear();
+        }
+
+        private void AddProductToOrder()
+        {
+            var newOrderItem = new OrderItem { Quantity = 1 };
+            var newLine = new ProductLineViewModel(newOrderItem, ProductsList) { Position = ProductLines.Count + 1 };
+            ProductLines.Add(newLine);
+        }
+
+        public void UpdatePositions()
+        {
+            for (int i = 0; i < ProductLines.Count; i++)
+                ProductLines[i].Position = i + 1;
+        }
+
+        private void CloseWindow()
+        {
+            // throw new NotImplementedException();
+        }
+
+        private void SaveOrder()
+        {
+            // throw new NotImplementedException();
         }
     }
 }
---
+++ MFormatik/ViewModels/ProductVms/ProductLineViewModel.cs
@@ -0,0 +1,122 @@
+﻿using MFormatik.Application.Helpers;
+using MFormatik.Core.Models;
+
+namespace MFormatik.ViewModels.ProductVms
+{
+    public class ProductLineViewModel : BaseViewModel
+    {
+        public OrderItem OrderItem { get; }
+        public IReadOnlyList<Product> Products { get; }
+
+        private Product _selectedProduct;
+        public Product SelectedProduct
+        {
+            get => _selectedProduct;
+            set
+            {
+                if (_selectedProduct != value)
+                {
+                    _selectedProduct = value;
+                    OrderItem.ProductId = _selectedProduct?.Id ?? 0;
+                    ProductUnitPrice = _selectedProduct.UnitPrice;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(ProductName));
+                    OnPropertyChanged(nameof(ProductUnitPrice));
+                }
+            }
+        }
+
+        public string ProductName => SelectedProduct?.Name ?? "No Product";
+
+        public decimal ProductUnitPrice
+        {
+            get => UnitPrice;
+            set
+            {
+                UnitPrice = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int ProductId
+        {
+            get => OrderItem.ProductId;
+            set
+            {
+                OrderItem.ProductId = value;
+                OnPropertyChanged(nameof(ProductId));
+                OnPropertyChanged(nameof(ProductName));
+            }
+        }
+
+
+        public int Quantity
+        {
+            get => OrderItem.Quantity;
+            set
+            {
+                OrderItem.Quantity = value;
+                OnPropertyChanged(nameof(UnitPrice));
+                OnPropertyChanged(nameof(NetPrice));
+            }
+        }
+        public decimal UnitPrice
+        {
+            get
+            {
+                return OrderCalculationHelper.CalculateAmount(OrderItem.UnitPrice, Quantity);
+            }
+            set
+            {
+                OrderItem.UnitPrice = value;
+                OnPropertyChanged(nameof(UnitPrice));
+                OnPropertyChanged(nameof(NetPrice));
+            }
+        }
+        public decimal? DiscountRate
+        {
+            get => OrderItem.DiscountRate;
+            set
+            {
+                OrderItem.DiscountRate = value; OnPropertyChanged(nameof(DiscountRate));
+                OnPropertyChanged(nameof(NetPrice));
+            }
+        }
+        public int Position
+        {
+            get => OrderItem.Position;
+            set
+            {
+                OrderItem.Position = value;
+                OnPropertyChanged(nameof(Position));
+            }
+        }
+
+        // 
+        public decimal NetPrice
+        {
+            set
+            {
+                NetPrice = value;
+                OnPropertyChanged(nameof(NetPrice));
+            }
+            get
+            {
+                return OrderCalculationHelper.CalculateNetAmount(UnitPrice, DiscountRate ?? 0);
+                //var discount = (DiscountRate ?? 0) / 100m;
+                //return Quantity * UnitPrice * (1 - discount);
+            }
+        }
+
+
+        public ProductLineViewModel(OrderItem orderItem, IReadOnlyList<Product> products)
+        {
+            OrderItem = orderItem;
+            Products = products;
+
+            _selectedProduct = new();
+            _selectedProduct = Products.FirstOrDefault(p => p.Id == OrderItem.ProductId);
+        }
+
+    }
+}
